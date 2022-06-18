458816d4af006cb5463a6eb8061981359471a269
Kartik <kartikjoshi267@gmail.com>
2022-06-18 14:46:01 +0530

    js file created

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+const card_question = document.querySelectorAll(".question");
+
+card_question.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        const answer_div = elem.nextElementSibling;
+        const button_div = elem.lastElementChild;
+
+        if (answer_div.style.maxHeight) {
+            button_div.innerHTML = '+';
+            answer_div.style.maxHeight = null;
+        } else {
+            button_div.innerHTML = '&#215';
+            answer_div.style.maxHeight = answer_div.scrollHeight + "px";
+          }
+    });
+});
